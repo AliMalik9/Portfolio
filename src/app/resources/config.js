a3b4defb0cab3be81c4f10ee05577e4dab13f75a
Ali Malik <91459530+AliMalik9@users.noreply.github.com>
2024-11-29 11:48:13 +0500

    22

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -20,7 +20,7 @@ const routes = {
 // Enable password protection on selected routes
 // Set password in pages/api/authenticate.ts
 const protectedRoutes = {
-    // '/work/wealthwave-modern-finance-app': true
+    // '/work/localhands-a-job-platform': true
 }
 
 const effects = {
