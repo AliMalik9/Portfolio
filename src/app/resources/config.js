e2fdf505368e95d19a9958817586c04118a41f12
Ali Malik <91459530+AliMalik9@users.noreply.github.com>
2024-11-29 12:09:43 +0500

    19

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -20,7 +20,7 @@ const routes = {
 // Enable password protection on selected routes
 // Set password in pages/api/authenticate.ts
 const protectedRoutes = {
-    // '/work/localhands-a-job-platform': true
+    '/work/localhands-a-job-platform': true
 }
 
 const effects = {
