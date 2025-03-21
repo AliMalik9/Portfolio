b897f6d597d956d545749342fbad2119d893bc81
Ali Malik <91459530+AliMalik9@users.noreply.github.com>
2025-03-21 21:50:29 +0000

    bit

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -41,9 +41,9 @@ const effects = {
 
 const style = {
     theme:       'dark',         // dark | light
-    neutral:     'sand',         // sand | gray | slate
-    brand:       'orange',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-    accent:      'yellow',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    neutral:     'gray',         // sand | gray | slate
+    brand:       'green',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    accent:      'aqua',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
     solid:       'contrast',     // color | contrast
     solidStyle:  'flat',         // flat | plastic
     border:      'playful',      // rounded | playful | conservative
